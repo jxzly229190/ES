8a0856a8ba99c272f4170ff2d2678daab2619658
JimmyZhang <jx-zly@live.cn>
2015-03-15 19:58:45 +0800

    added the login page on the server side

---
+++ ES.Server/Login.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ES.Server
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Login1Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            var db = new dbDataContext();
+            var user = db.LoginUsers.SingleOrDefault(u => u.Name == Login1.UserName && u.Pwd == Login1.Password);
+            if (user!=null)
+            {
+                user.LastLogin = DateTime.Now;
+                db.SubmitChanges();
+                Response.Redirect("NewClient.aspx");
+            }
+            else
+            {
+                Login1.FailureText = "用户名或者密码错误，请确认后重新输入";
+                Login1.UserName = "";
+            }
+        }
+    }
+}
---
+++ ES.Server/NewClient.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ES.Server
+{
+    public partial class NewClient : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
