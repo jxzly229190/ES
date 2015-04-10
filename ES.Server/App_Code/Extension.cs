2701607fa7c33040ef36b7813f65e7641ccc4f09
JimmyZhang <jx-zly@live.cn>
2015-04-11 00:41:21 +0800

    添加extension文件

---
+++ ES.Server/App_Code/Extension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ES.Server
+{
+    public static class Extension
+    {
+        public static void AddErrors(this Controller c, params string[] errors)
+        {
+            foreach (var error in errors)
+            {
+                c.ModelState.AddModelError("", error);
+            }
+        }
+    }
+}
