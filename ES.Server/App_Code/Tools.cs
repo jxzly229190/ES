d80a47b2d5b76411e4c586e16dfc8c8bd7066206
JimmyZhang <jx-zly@live.cn>
2015-04-11 11:11:59 +0800

    完善门店明细资料页面

--- ES.Server/App_Code/Tools.cs
+++ ES.Server/App_Code/Tools.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -6,11 +7,24 @@ namespace ES.Server
 {
     public class Tools
     {
-        public static IList<SelectListItem> GetStatusByTableName(string tableName, string colName, string selectedValue)
+        public static IList<SelectListItem> GetKeyValuesByTableColumn(string tableName, string colName, string selectedValue)
         {
             var keyValues = new Entities().Meta_KeyValue.Where(m => m.TableName == tableName && m.ColName == colName).ToList();
 
             return keyValues.Select(kv => new SelectListItem() {Text = kv.Text, Value = kv.Value, Selected = kv.Value == selectedValue}).OrderBy(m=>m.Value).ToList();
-        }  
+        }
+
+        public static string GetSelectedTextByTableColumn(string tableName, string colName, string selectedValue)
+        {
+            var kvs =
+                new Entities().Meta_KeyValue.Where(
+                    m => m.TableName == tableName && m.ColName == colName && m.Value == selectedValue);
+            if (kvs.Any())
+            {
+                return kvs.First().Text;
+            }
+
+            return null;
+        }
     }
 }
