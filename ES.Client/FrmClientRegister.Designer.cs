3a0e163c9ba7de12a7ed099299c0019c8a467537
jxzly229190 <jx-zly@live.cn>
2015-05-14 10:13:58 +0800

    update

---
+++ ES.Client/FrmClientRegister.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ES.Client
+{
+    partial class FrmClientRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.txtGuid = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.btnSave = new DevExpress.XtraEditors.SimpleButton();
+            this.btnClose = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.txtGuid.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(30, 73);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(37, 13);
+            this.labelControl2.TabIndex = 0;
+            this.labelControl2.Text = "GUID：";
+            // 
+            // txtGuid
+            // 
+            this.txtGuid.Location = new System.Drawing.Point(73, 70);
+            this.txtGuid.Name = "txtGuid";
+            this.txtGuid.Size = new System.Drawing.Size(284, 20);
+            this.txtGuid.TabIndex = 1;
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold);
+            this.labelControl3.Location = new System.Drawing.Point(139, 22);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(100, 23);
+            this.labelControl3.TabIndex = 2;
+            this.labelControl3.Text = "客户端注册";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Appearance.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.btnSave.Appearance.Options.UseFont = true;
+            this.btnSave.Location = new System.Drawing.Point(83, 120);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "保存";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Appearance.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.btnClose.Appearance.Options.UseFont = true;
+            this.btnClose.Location = new System.Drawing.Point(227, 120);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "关闭";
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // FrmClientRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 168);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.txtGuid);
+            this.Controls.Add(this.labelControl2);
+            this.Name = "FrmClientRegister";
+            this.Text = "客户端注册";
+            ((System.ComponentModel.ISupportInitialize)(this.txtGuid.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.TextEdit txtGuid;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.SimpleButton btnSave;
+        private DevExpress.XtraEditors.SimpleButton btnClose;
+    }
+}
---
+++ ES.Client/FrmClientRegister.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ES.Client
+{
+    public partial class FrmClientRegister : Form
+    {
+        dbDataContext db = new dbDataContext();
+        Client client = null;
+
+        public FrmClientRegister()
+        {
+            InitializeComponent();
+            InitData();
+        }
+
+        private void InitData() {
+            client = db.Client.FirstOrDefault(c => c.IsCurrent == true && c.Type == 1);
+            if (client != null) {
+                txtGuid.Text = client.GUID.ToString();
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtGuid.Text)) {
+                MessageBox.Show("请输入GUID。");
+                return;
+            }
+
+            Guid guid;
+            if (!Guid.TryParse(txtGuid.Text, out guid))
+            {
+                MessageBox.Show("请输入正确的Guid。");
+                return;
+            }
+
+            if (client == null)
+            {
+                client = new Client();
+                client.GUID = guid;
+                client.Type = 1;
+                client.IsCurrent = true;
+                db.Client.InsertOnSubmit(client);
+            }
+            else {
+                client.GUID = guid;
+                client.Type = 1;
+                client.IsCurrent = true;
+            }            
+
+            try
+            {
+                db.SubmitChanges();
+                MessageBox.Show("保存成功");
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("您确定要关闭吗？", "提示", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
