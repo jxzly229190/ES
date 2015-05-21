bd325f90549a6b1f1d3b4b105a29f69b32faf024
JimmyZhang <jx-zly@live.cn>
2015-05-21 20:35:25 +0800

    清除未使用到的Using

--- ES.Server/Controllers/TranConfigsController.cs
+++ ES.Server/Controllers/TranConfigsController.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using ES.Server;
-using ES.Server.Models;
 
 namespace ES.Server.Controllers
 {
